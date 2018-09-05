4e68a06b4014d1bd79613cc9b5913c12904bc735
SC Van Nostrand <scvannost@gmail.com>
2018-09-05 14:51:39 -0400

    Create helpers.py
    Import script to have everything I need in a one line import

---
+++ helpers.py
@@ -0,0 +1,15 @@
+import numpy as np
+import pandas as pd
+import scipy as sp
+import seaborn as sns
+import sklearn as sk
+import matplotlib.pyplot as plt
+import pickle
+
+from printer import * # see github.com/scvannost/randompy
+
+from matplotlib import rc
+rc('text',usetex=True)
+del rc
+
+pd.set_option('display.latex.repr',True)
